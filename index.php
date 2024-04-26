d2e8735a615365e38281ed62583694ef3ab28c5a
Bamuel <me@bamuel.com>
2024-04-26 12:10:20 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>S and E Wedding Photo Collection</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/htmx.org@1.9.12" integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2" crossorigin="anonymous"></script>
+</head>
+<body>
+<h1>S and E's wedding photo collection</h1>
+<p>Hello all you lovely people, we are collecting photos that people took at our wedding, so if you have pictures please add them here!</p>
+<p>Please add your name, so we know who they have come from. Upload your pictures with the file picker below, (you can add multiple pictures at the same time) if you have many be patient with the upload times!</p>
+</body>
+</html>
+
