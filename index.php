ed794fb2a29845c43b98cadc0195f551d7c2ed5d
Bamuel <me@bamuel.com>
2024-05-02 12:28:03 +1000

    Using a Masonry Layout, Looks better now.

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ $json_data = json_decode($json, true);
 <!-- Gallery -->
 <div class="container">
     <h2>Gallery</h2>
-    <div class="row">
+    <div class="row" data-masonry='{"percentPosition": true, "animate": false}'>
         <?php
         if (!file_exists('savedimages')) {
             @mkdir('savedimages', 0777, true);
@@ -196,8 +196,21 @@ $json_data = json_decode($json, true);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 <script src="fileinput/fileinput.min.js"></script>
 <script src="fileinput/themes/explorer-fa6/theme.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 <script>
     $(document).ready(function () {
+
+        // Run initially after 500ms
+        //This will re-layout the images after they've loaded, well constantly check every 1500ms.
+        //This is needed for videos to display correctly in the masonry layout after they've loaded.
+        setTimeout(function() {
+            $('.row').masonry('layout');
+            // Run every 1.5 seconds after the initial run
+            setInterval(function() {
+                $('.row').masonry('layout');
+            }, 1500);
+        }, 500);
+
         $("#files").fileinput({
             theme: "explorer-fa6",
             allowedFileTypes: ['image', 'video'],
