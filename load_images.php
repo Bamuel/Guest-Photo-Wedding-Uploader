b930272bbf14a4feb70fedfde733d0b02fbdd73e
Samuel <me@bamuel.com>
2024-06-03 12:50:15 +1000

    Added DJ

--- load_images.php
+++ load_images.php
@@ -60,6 +60,9 @@ if (file_exists('savedimages')) {
         else if (in_array(strtolower($userName), $json_data['photographers'], true)) {
             $icon = 'fa-solid fa-camera-retro fa-lg';
         }
+        else if (in_array(strtolower($userName), $json_data['dj'], true)) {
+            $icon = 'fa-solid fa-headphones fa-lg';
+        }
         else {
             $icon = 'fa-solid fa-user fa-lg';
         }
